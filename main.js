99f3a2a3f01831e4b8322e998f0cea7dcb0ebc1e
Elkin Gutierrez <nawassedd@gmail.com>
2026-06-22 10:11:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+  const themeToggle = document.getElementById("themeToggle");
+  const themeIcon = document.getElementById("themeIcon");
+
+  // Se revisa el tema guardado anteriormente
+  const savedTheme = localStorage.getItem("temaModaEcci");
+
+  // Por defecto la página inicia clara
+  if (savedTheme === "dark") {
+    activarModoOscuro();
+  } else {
+    activarModoClaro();
+  }
+
+  themeToggle.addEventListener("click", () => {
+    if (document.body.classList.contains("dark-theme")) {
+      activarModoClaro();
+    } else {
+      activarModoOscuro();
+    }
+  });
+
+  function activarModoOscuro() {
+    document.body.classList.add("dark-theme");
+
+    themeIcon.classList.remove("bi-moon-stars-fill");
+    themeIcon.classList.add("bi-sun-fill");
+
+    localStorage.setItem("temaModaEcci", "dark");
+  }
+
+  function activarModoClaro() {
+    document.body.classList.remove("dark-theme");
+
+    themeIcon.classList.remove("bi-sun-fill");
+    themeIcon.classList.add("bi-moon-stars-fill");
+
+    localStorage.setItem("temaModaEcci", "light");
+  }
+});
